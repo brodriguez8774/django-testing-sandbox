f3df265ca391dc9332288e8eaddc62d55ebfd9e5
Brandon Rodriguez <brodriguez8774@gmail.com>
2023-02-27 18:05:45 -0500

    Add start of basic login/permission check views in each project

--- django_v2/test_app/views.py
+++ django_v2/test_app/views.py
@@ -3,8 +3,11 @@ Views for Django v2.2 test project app.
 """
 
 # Third-Party Imports.
+from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render
 
+
+# region Index/Root Views
 
 def root_project_home_page(request):
     """Shown for root page of entire project."""
@@ -12,5 +15,27 @@ def root_project_home_page(request):
 
 
 def index(request):
-    """"""
+    """Test app index page."""
     return render(request, 'test_app/index.html')
+
+# endregion Index/Root Views
+
+
+# region Login/Permission Test Views
+
+@login_required
+def view_with_login_check(request):
+    """Test view with basic login check."""
+    return render(request, 'test_app/index.html')
+
+@permission_required('TODO: Add permission check')
+def view_with_permission_check(request):
+    """Test view with basic User permission check."""
+    return render(request, 'test_app/index.html')
+
+# TODO: Add group check.
+def view_with_group_check(request):
+    """Test view with basic User group check."""
+    return render(request, 'test_app/index.html')
+
+# endregion Login/Permission Test Views
