9d3bdf30296b91856182c5be55d511ad73a9fa99
Brandon Rodriguez <brodriguez8774@gmail.com>
2023-02-27 16:30:33 -0500

    Correct bad references to "mysite" in each project settings/setup files

--- django_v4/settings/urls.py
+++ django_v4/settings/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""Django v4.1 test project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
