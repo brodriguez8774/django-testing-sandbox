9e710ead0b16ebe3eef5e11543979813c47b6d12
Brandon Rodriguez <brodriguez8774@gmail.com>
2023-02-27 16:45:47 -0500

    Correct all projects to serve initial app page

--- django_v2/settings/settings.py
+++ django_v2/settings/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Project apps.
+    'test_app.apps.TestAppConfig',
+
+    # Built-in Django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
