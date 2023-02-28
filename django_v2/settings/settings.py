bafee728143afade9bb76d5e2934eac79375d5ed
Brandon Rodriguez <brodriguez8774@gmail.com>
2023-02-28 01:57:29 -0500

    Add some very basic test models for all projects

--- django_v2/settings/settings.py
+++ django_v2/settings/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'test_app.User'
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
--- django_v2/test_app/models.py
+++ django_v2/test_app/models.py
@@ -3,6 +3,40 @@ Models for Django v2.2 test project app.
 """
 
 # Third-Party Imports.
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from localflavor.us.models import USStateField, USZipCodeField
 
-# Create your models here.
+
+MAX_LENGTH = 255
+
+
+class User(AbstractUser):
+    """Custom user model definition.
+    Defined as per the Django docs. Not yet directly used.
+    """
+    pass
+
+
+class UserProfile(models.Model):
+    """Basic model to act as a test fk to user model."""
+
+    # Relationship Keys.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Model Fields.
+    address_1 = models.CharField(max_length=MAX_LENGTH, blank=True)
+    address_2 = models.CharField(max_length=MAX_LENGTH, blank=True)
+    city = models.CharField(max_length=MAX_LENGTH, blank=True)
+    state = USStateField()
+    zipcode = USZipCodeField()
+
+
+class FavoriteFood(models.Model):
+    """Basic model to act as a test m2m relation to user model."""
+
+    # Relationship Keys.
+    user = models.ManyToManyField(User)
+
+    # Model fields.
+    name = models.CharField(max_length=MAX_LENGTH)
